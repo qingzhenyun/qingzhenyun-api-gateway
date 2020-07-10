43f1204e3f0bfc8d970c7aecfae09865b676b13d
qingzhenyun <56344693+qingzhenyun@users.noreply.github.com>
2020-07-10 16:54:30 +0800

    Update mongodb_config.js

--- src/app/config/mongodb_config.js
+++ src/app/config/mongodb_config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'connectionString': 'mongodb://xiaoxiongweini@192.168.250.13:27017',
+    'connectionString': 'mongodb://-@192.168.250.13:27017',
     'defaultDb' : 'default'
 }
